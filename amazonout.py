c9dfd2f2eeb9e79fe930aea8faa1642c53500196
Jothi Lakshmi <85429413+123567-wq@users.noreply.github.com>
2024-07-22 21:50:01 +0530

    Add files via upload

---
+++ amazonout.py
@@ -0,0 +1,19 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:\Users\jrajendran\Downloads\ecomm_python.py ============
+INFO:root:Amazon Search Response Status Code: 503
+ERROR:root:Amazon request failed with status code 503
+INFO:root:Retrying in 5 seconds...
+INFO:root:Amazon Search Response Status Code: 200
+INFO:root:Amazon Search Response Data: []
+>>> 
+============ RESTART: C:\Users\jrajendran\Downloads\ecomm_python.py ============
+INFO:root:Amazon Search Response Status Code: 503
+ERROR:root:Amazon request failed with status code 503
+INFO:root:Retrying in 5 seconds...
+INFO:root:Amazon Search Response Status Code: 503
+ERROR:root:Amazon request failed with status code 503
+INFO:root:Retrying in 5 seconds...
+INFO:root:Amazon Search Response Status Code: 200
+INFO:root:Amazon Search Response Data: []
